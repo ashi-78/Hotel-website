4dc22f2c5afc7bc8b4c30ce57753a23d09b03d1b
Ashi Kashyap <ashikashyap131@gmail.com>
2025-08-07 17:29:05 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
-import { DarkModeContextProvider } from "./context/darkModeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SearchContextProvider>
-        <DarkModeContextProvider>
-          <App />
-        </DarkModeContextProvider>
+        <App />
       </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
