ed4d16fe646cb2741d9f6fd443e8c4b4da9c9348
Ashi Kashyap <ashikashyap131@gmail.com>
2025-08-07 17:35:59 +0530

    Fix: missing darkModeContext import

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,19 @@ import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-      </SearchContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <AuthContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AuthContextProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Make sure you have <div id='root'></div> in your index.html");
+}
